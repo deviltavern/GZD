37873e13bb85d7e373affe665504d251e31e124e
xinshou1106 <906892826@qq.com>
2019-04-25 20:15:31 +0800

    更新xml

--- Assets/Scripts/ResourcesManager/ResName.cs
+++ Assets/Scripts/ResourcesManager/ResName.cs
@@ -14,4 +14,28 @@ public class ResName : MonoBehaviour {
     public const string AimPointMateril = "AimPoint";
     public const string OnGetOriginMateril = "OnGetOrigin";
     public const string hammer = "hammer";
+
+
+    //卡板
+      public const string bp_lenInput = "bp_lenInput";
+      public const string bp_widthInput = "bp_widthInput";
+      public const string bp_heightInput = "bp_heightInput";
+      public const string bp_weightInput = "bp_weightInput";
+
+    //产品
+      public const string p_lenInput = "p_lenInput";
+      public const string p_widthInput = "p_widthInput";
+      public const string p_heightInput = "p_heightInput";
+      public const string p_weightInput = "p_weightInput";
+      public const string p_disInput = "p_disInput";
+      public const string p_numInput = "p_numInput";
+      public const string p_labelDropdown = "p_labelDropdown";
+
+    //通讯配置
+      public const string s_ipInput = "s_ipInput";
+      public const string s_portInput = "s_portInput";
+      public const string s_kindInput = "s_kindInput";
+
+      public const string config = "config"; 
+
 }
